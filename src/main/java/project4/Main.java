cef314d50cdef166dc70b24c9e80600b624fb97f
gadirlih <gadirlih@gmail.com>
2021-04-26 05:25:08 +0400

    reformat code

--- src/main/java/project4/Main.java
+++ src/main/java/project4/Main.java
@@ -21,118 +21,118 @@ public class Main {
     double[][][] qTable = new double[40][40][4];
 
     public void qLearning(int[] state) throws IOException {
-            // state has x and y coordinates on the grid
-            boolean done = false;
+        // state has x and y coordinates on the grid
+        boolean done = false;
 
-            // make it while loop it should continue till traversing ends
-            while(!done){
-                double expirationRateThreshold = Math.random();
-                int action;
-                if(expirationRateThreshold > explorationRate){
-                    // chose action based on the max qvalue of the state
-                    double max = 0;
-                    int maxIndex = 0;
-                    for(int  k = 0; k < 4; k++){
-                        if(qTable[state[0]][state[1]][k] > max){
-                            max = qTable[state[0]][state[1]][k];
-                            maxIndex = k;
-                        }
-                    }
-                    action = maxIndex;
-                }else{
-                    // else explore other actions
-                    // chose random number between 0 and 3 inclusive
-                    Random rand = new Random();
-                    action = rand.nextInt(4);
-                }
-                System.out.println("New Action: " + action);
-
-                // determine move from current state and action
-                int[] move = state;
-                switch (action){
-                    case 0: // move left
-                        move = new int[]{state[0], state[1] - 1};
-                        break;
-                    case 1: // move right
-                        move = new int[]{state[0], state[1] + 1};
-                        break;
-                    case 2: // move up
-                        move = new int[]{state[0] - 1, state[1]};
-                        break;
-                    case 3: // move down
-                        move = new int[]{state[0] + 1, state[1]};
-                        break;
-                }
-                System.out.println("Current State: " + state[0] + " , " + state[1]);
-                System.out.println("Move: " + move[0] + " , " + move[1]);
-
-                // Make a move
-                // API will return new state and reward
-                // TODO MAKE API CALL TO MAKE MOVE - DONE
-                int[] new_state = new int[2];
-
-                OkHttpClient moveClient = new OkHttpClient().newBuilder()
-                        .build();
-                MediaType mediaType = MediaType.parse("text/plain");
-                RequestBody moveBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                        .addFormDataPart("move",move[0] + "," + move[1])
-                        .addFormDataPart("type","move")
-                        .addFormDataPart("teamId","1258")
-                        .addFormDataPart("worldId", String.valueOf(WORLD_ID))
-                        .build();
-                Request moveRequest = new Request.Builder()
-                        .url("https://www.notexponential.com/aip2pgaming/api/rl/gw.php")
-                        .method("POST", moveBody)
-                        .addHeader("x-api-key", "998bc59f37ed76e14f08")
-                        .addHeader("userid", "1060")
-                        .build();
-
-                Response moveResponse = moveClient.newCall(moveRequest).execute();
-                JSONObject moveResponseJSON = new JSONObject(moveResponse.body().string());
-//                if(responseJSON.getInt("world") == -1) done = true;
-                double reward = moveResponseJSON.getDouble("reward");
-                new_state[0] = moveResponseJSON.getJSONObject("newState").getInt("x");
-                new_state[1] = moveResponseJSON.getJSONObject("newState").getInt("y");
-
-                System.out.println("Reward: " + reward);
-                System.out.println("New State: " + new_state[0] + " , " + new_state[1]);
-
-                // Update qTable
-                // first find the max qValue of the new state
-                double new_state_max = 0;
-                for(int  k = 0; k < 4; k++){
-                    if(qTable[new_state[0]][new_state[1]][k] > new_state_max){
-                        new_state_max = qTable[new_state[0]][new_state[1]][k];
+        // make it while loop it should continue till traversing ends
+        while (!done) {
+            double expirationRateThreshold = Math.random();
+            int action;
+            if (expirationRateThreshold > explorationRate) {
+                // chose action based on the max qvalue of the state
+                double max = 0;
+                int maxIndex = 0;
+                for (int k = 0; k < 4; k++) {
+                    if (qTable[state[0]][state[1]][k] > max) {
+                        max = qTable[state[0]][state[1]][k];
+                        maxIndex = k;
                     }
                 }
-                // Update qTable of the current state
-                qTable[state[0]][state[1]][action] = qTable[state[0]][state[1]][action] * (1 - learningRate) +
-                        learningRate * (reward + discountRate * new_state_max);
-
-                state = new_state;
-
-                // check if the game is over
-                // TODO check with api to see if it is over - DONE
-                // done = true; if it is over
-                // if returned world is -1 then it is over
-                OkHttpClient locationClient = new OkHttpClient().newBuilder()
-                        .build();
-                Request locationRequest = new Request.Builder()
-                        .url("https://www.notexponential.com/aip2pgaming/api/rl/gw.php?type=location&teamId=1258")
-                        .method("GET", null)
-                        .addHeader("x-api-key", "998bc59f37ed76e14f08")
-                        .addHeader("userid", "1060")
-                        .build();
-                Response locationResponse = locationClient.newCall(locationRequest).execute();
-                JSONObject responseJSON = new JSONObject(locationResponse.body().string());
-                int activeWorld = responseJSON.getInt("world");
-                if(activeWorld == -1) done = true;
-//                done = true;
-                System.out.println("Active World: " + activeWorld);
-                sleepSec(15);
+                action = maxIndex;
+            } else {
+                // else explore other actions
+                // chose random number between 0 and 3 inclusive
+                Random rand = new Random();
+                action = rand.nextInt(4);
             }
+            System.out.println("New Action: " + action);
 
-            // decay the exploration rate
+            // determine move from current state and action
+            int[] move = state;
+            switch (action) {
+                case 0: // move left
+                    move = new int[]{state[0], state[1] - 1};
+                    break;
+                case 1: // move right
+                    move = new int[]{state[0], state[1] + 1};
+                    break;
+                case 2: // move up
+                    move = new int[]{state[0] - 1, state[1]};
+                    break;
+                case 3: // move down
+                    move = new int[]{state[0] + 1, state[1]};
+                    break;
+            }
+            System.out.println("Current State: " + state[0] + " , " + state[1]);
+            System.out.println("Move: " + move[0] + " , " + move[1]);
+
+            // Make a move
+            // API will return new state and reward
+            // TODO MAKE API CALL TO MAKE MOVE - DONE
+            int[] new_state = new int[2];
+
+            OkHttpClient moveClient = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody moveBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                    .addFormDataPart("move", move[0] + "," + move[1])
+                    .addFormDataPart("type", "move")
+                    .addFormDataPart("teamId", "1258")
+                    .addFormDataPart("worldId", String.valueOf(WORLD_ID))
+                    .build();
+            Request moveRequest = new Request.Builder()
+                    .url("https://www.notexponential.com/aip2pgaming/api/rl/gw.php")
+                    .method("POST", moveBody)
+                    .addHeader("x-api-key", "998bc59f37ed76e14f08")
+                    .addHeader("userid", "1060")
+                    .build();
+
+            Response moveResponse = moveClient.newCall(moveRequest).execute();
+            JSONObject moveResponseJSON = new JSONObject(moveResponse.body().string());
+//                if(responseJSON.getInt("world") == -1) done = true;
+            double reward = moveResponseJSON.getDouble("reward");
+            new_state[0] = moveResponseJSON.getJSONObject("newState").getInt("x");
+            new_state[1] = moveResponseJSON.getJSONObject("newState").getInt("y");
+
+            System.out.println("Reward: " + reward);
+            System.out.println("New State: " + new_state[0] + " , " + new_state[1]);
+
+            // Update qTable
+            // first find the max qValue of the new state
+            double new_state_max = 0;
+            for (int k = 0; k < 4; k++) {
+                if (qTable[new_state[0]][new_state[1]][k] > new_state_max) {
+                    new_state_max = qTable[new_state[0]][new_state[1]][k];
+                }
+            }
+            // Update qTable of the current state
+            qTable[state[0]][state[1]][action] = qTable[state[0]][state[1]][action] * (1 - learningRate) +
+                    learningRate * (reward + discountRate * new_state_max);
+
+            state = new_state;
+
+            // check if the game is over
+            // TODO check with api to see if it is over - DONE
+            // done = true; if it is over
+            // if returned world is -1 then it is over
+            OkHttpClient locationClient = new OkHttpClient().newBuilder()
+                    .build();
+            Request locationRequest = new Request.Builder()
+                    .url("https://www.notexponential.com/aip2pgaming/api/rl/gw.php?type=location&teamId=1258")
+                    .method("GET", null)
+                    .addHeader("x-api-key", "998bc59f37ed76e14f08")
+                    .addHeader("userid", "1060")
+                    .build();
+            Response locationResponse = locationClient.newCall(locationRequest).execute();
+            JSONObject responseJSON = new JSONObject(locationResponse.body().string());
+            int activeWorld = responseJSON.getInt("world");
+            if (activeWorld == -1) done = true;
+//                done = true;
+            System.out.println("Active World: " + activeWorld);
+            sleepSec(15);
+        }
+
+        // decay the exploration rate
 //            exploration_rate -= 0.2;
 
 
@@ -155,7 +155,7 @@ public class Main {
         return array;
     }
 
-    public  void sleepSec(int second) {
+    public void sleepSec(int second) {
         try {
             System.out.println("start sleeping");
             sleep(second * 1000);
